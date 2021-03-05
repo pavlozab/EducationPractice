458349c514803503ddb2fe41401a2006a596e41c
pavlozab <pavlo.zabavskyi@lnu.edu.ua>
2021-03-05 16:29:10 +0200

    NLog

--- C#/Task_03/ProductRest/ProductRest/Controllers/ProductController.cs
+++ C#/Task_03/ProductRest/ProductRest/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using ProductRest.Dtos;
 using ProductRest.Repositories;
 
@@ -13,93 +14,149 @@ namespace ProductRest.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IProductsRepository _repository;
+        private readonly ILogger<ProductController> _logger;
         
-        public ProductController(IProductsRepository repository)
+        public ProductController(IProductsRepository repository, ILogger<ProductController> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
         
         // GET /products
         [HttpGet]
-        public async Task<IEnumerable<ProductDto>> GetProducts()
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetProducts()
         {
-            var items = (await _repository.GetProductsAsync())
-                                            .Select(item => item.AsDto());
-            return items;
+            try
+            {
+                var items = (await _repository.GetProductsAsync())
+                    .Select(item => item.AsDto());
+                
+                _logger.LogInformation("Returned all products.");
+                return Ok(items);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Something went wrong inside GetProducts action: {0}", e.Message);
+                return StatusCode(500, "Internal server error.");
+            }
         }
 
         // GET /products/{id}
         [HttpGet("{id}")]
         public async Task<ActionResult<ProductDto>> GetProduct(Guid id)
         {
-            var item = await _repository.GetProductAsync(id);
+            try
+            {
+                var item = await _repository.GetProductAsync(id);
 
-            if (item is null)
-                return NotFound();
-
-            return item.AsDto();  // Ok()
+                if (item is null)
+                {
+                    _logger.LogInformation("Product with id: {0}, hasn't been found.", id);
+                    return NotFound();
+                }
+                
+                _logger .LogInformation("Returned product with id: {0}", id);
+                return Ok(item.AsDto());
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Something went wrong inside GetProduct action: {0}", e.Message); 
+                return StatusCode(500, "Internal server error"); 
+            }
         }
 
         // POST /products
         [HttpPost]
         public async Task<ActionResult<ProductDto>> CreateProduct(CreateProductDto itemDto)
         {
-            ProductDto item = new()
+            try
             {
-                Id = Guid.NewGuid(),
-                AddressLine = itemDto.AddressLine,
-                PostalCode = itemDto.PostalCode,
-                Country = itemDto.Country,
-                City = itemDto.City,
-                FaxNumber = itemDto.FaxNumber,
-                PhoneNumber = itemDto.PhoneNumber
-            };
+                ProductDto item = new()
+                {
+                    Id = Guid.NewGuid(),
+                    AddressLine = itemDto.AddressLine,
+                    PostalCode = itemDto.PostalCode,
+                    Country = itemDto.Country,
+                    City = itemDto.City,
+                    FaxNumber = itemDto.FaxNumber,
+                    PhoneNumber = itemDto.PhoneNumber
+                };
             
-            await _repository.CreateProductAsync(item);
+                await _repository.CreateProductAsync(item);
 
-            return CreatedAtAction(nameof(GetProduct), new { id = item.Id }, item.AsDto()); 
+                _logger.LogInformation("Created product.");
+                return CreatedAtAction(nameof(GetProduct), new { id = item.Id }, item.AsDto()); 
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Something went wrong inside CreateProduct action: {0}", e.Message); 
+                return StatusCode(500, "Internal server error"); 
+            }
+            
         }
 
         // PUT /products/{id}
         [HttpPut("{id}")]
         public async Task<ActionResult> UpdateProduct(Guid id, CreateProductDto itemDto)
         {
-            var existingItem = await  _repository.GetProductAsync(id);
-
-            if (existingItem is null)
+            try
             {
-                return NotFound();
-            }
+                var existingItem = await  _repository.GetProductAsync(id);
 
-            ProductDto updatedItem = existingItem with
-            {
-                AddressLine = itemDto.AddressLine,
-                PostalCode = itemDto.PostalCode,
-                Country = itemDto.Country,
-                City = itemDto.City,
-                FaxNumber = itemDto.FaxNumber,
-                PhoneNumber = itemDto.PhoneNumber
-            };
+                if (existingItem is null)
+                {
+                    _logger.LogInformation("Product with id: {0}, hasn't been found.", id);
+                    return NotFound();
+                }
+
+                ProductDto updatedItem = existingItem with
+                {
+                    AddressLine = itemDto.AddressLine,
+                    PostalCode = itemDto.PostalCode,
+                    Country = itemDto.Country,
+                    City = itemDto.City,
+                    FaxNumber = itemDto.FaxNumber,
+                    PhoneNumber = itemDto.PhoneNumber
+                };
             
-            await _repository.UpdateProductAsync(updatedItem);
+                await _repository.UpdateProductAsync(updatedItem);
 
-            return NoContent();
+                _logger.LogInformation("Updated product with id: {0}", id);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Something went wrong inside CreateProduct action: {0}", e.Message); 
+                return StatusCode(500, "Internal server error"); 
+            }
+            
         }
 
         // DELETE /products/{id}
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteProduct(Guid id)
         {
-            var existingItem = await _repository.GetProductAsync(id);
-
-            if (existingItem is null)
+            try
             {
-                return NotFound();
+                var existingItem = await _repository.GetProductAsync(id);
+
+                if (existingItem is null)
+                {
+                    _logger.LogInformation("Product with id: {0}, hasn't been found.", id);
+                    return NotFound();
+                }
+
+                await _repository.DeleteProductAsync(id);
+                
+                _logger.LogInformation("Deleted product with id: {0}", id);
+                return NoContent();
             }
-
-            await _repository.DeleteProductAsync(id);
-
-            return NoContent();
+            catch (Exception e)
+            {
+                _logger.LogError("Something went wrong inside CreateProduct action: {0}", e.Message); 
+                return StatusCode(500, "Internal server error"); 
+            }
+            
         }
     }
 }
--- C#/Task_03/ProductRest/ProductRest/Program.cs
+++ C#/Task_03/ProductRest/ProductRest/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace ProductRest
 {
@@ -13,11 +16,34 @@ namespace ProductRest
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>(); 
+                    
+                }).ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
